5acfb0a20efd81297b2657e5a794da00fc89cde5
Colman Currie <curriecolman@gmail.com>
2020-12-03 13:49:20 -0800

    begin forming the layout component

---
+++ src/components/Layout/Layouts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'; 
+
+const Layout = () => (
+    <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>
+        {response.children}
+    </main>
+    </Aux>
+)
