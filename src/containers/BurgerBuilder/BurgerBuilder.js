0a83f86e492fffc0cf00393a07e80c74c0cb1bf3
Colman Currie <curriecolman@gmail.com>
2020-12-09 11:24:44 -0800

    update all burger related files, spellcheck/format

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
     state = {
         ingredients: {
-            salad: 1, 
-            bacon: 1, 
-            cheese: 2,
-            meat: 2,
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
-    render() {
-        return(
+
+    render () {
+        return (
             <Aux>
-                <Burger ingredients={this.state.ingredients}/>
-                {/* {console.log(this.state.ingredients)} */}
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
