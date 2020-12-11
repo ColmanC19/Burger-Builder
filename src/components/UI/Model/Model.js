47523262be0478fa39e415fd1e891693f3f0ec13
Colman Currie <curriecolman@gmail.com>
2020-12-10 16:12:37 -0800

    add styling to model

--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Model.module.css';
 
 const model = (props) => (
-    <div>
+    <div className={classes.Model}>
         {props.children}
     </div>
 );
