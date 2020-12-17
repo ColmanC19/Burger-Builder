6c175a48e697af8e3d512e59ecf91827095532e0
Colman Currie <curriecolman@gmail.com>
2020-12-17 15:36:45 -0800

    add click listener onto backdrop in layout

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
     // ....
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+         <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={classes.SideDrawer}>
             <div className={classes.Logo}>
                 <Logo />
             </div> 
                 <nav>
                     <NavigationItems />
                 </nav>
-        </div>
+            </div>
+
+        </Aux>
     );
 };
 
