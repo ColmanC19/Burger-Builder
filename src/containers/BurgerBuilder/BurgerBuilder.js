fa08b0175a73f3f265db1f9bada707c2d9dbda94
Colman Currie <curriecolman@gmail.com>
2021-01-06 13:53:06 -0800

    connected to firebase via axios and dummy data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -99,7 +99,7 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/orders.json')
+        axios.post('/orders.json', order)
             .then(response => console.log(response))
             .catch(error => console.log(error));
     }
